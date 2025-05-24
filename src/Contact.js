e8fed75b550d156f49d5ecc168306efbaf9588d8
Kenji <k.ejiri.0412@gmail.com>
2025-05-24 10:29:32 +0900

    プロフィールおよび導入事例の修正

--- src/Contact.js
+++ src/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => (
   <section id="contact" className="contact">
     <h2>無料相談・技術的なご質問もお気軽に</h2>
     <div className="section-content">
-      <p>30秒で完了するフォームからご連絡ください。</p>
+      <p>以下のフォームからご連絡ください。</p>
       <ContactForm />
     </div>
   </section>
--- src/Works.js
+++ src/Works.js
@@ -14,8 +14,11 @@ const Works = () => (
     <h2>導入事例（一部）</h2>
     <div className="section-content">
       <ul>
+        <li>
+          自動運転ベンチャー：レーンキープ、アダプティブクルーズコントロール等の制御および自己位置推定機能の開発支援
+        </li>
         <li>大手自動車OEM：ADAS制御ブロックの開発支援</li>
-        <li>産業機械メーカー：Simulinkモデルのリファクタリングと検証自動化</li>
+        <li>産業機械メーカー：Simulinkモデルのリファクタリングと検証</li>
       </ul>
     </div>
   </section>
